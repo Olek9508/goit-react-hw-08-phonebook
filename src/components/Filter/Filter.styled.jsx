4b113bcc2b2e76c0d4a76228d9e99483a9acb729
Oleksandr Volik <polskasasha@gmail.com>
2023-01-27 22:06:16 +0200

    clean Filter.styled code

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -21,30 +21,6 @@ export const Input = styled.input`
 
 
 
-// export const Box = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     text-align: center;
-// `
-//     ;
 
-
-//     export const Label = styled.label`
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     padding-left:45px;
-//     `
-//     ;
-
-
-//     export const Input = styled.input`
-//     margin-top: 15px;
-//     border-radius: 5px;
-//     border: solid 1px;
-//     box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.08),
-//     0px 2px 2px rgba(0, 0, 0, 0.12);
-// `
-//     ;
 
 
