93361f2e2d8c2f821e35d7bc85857cea8f06c114
Oleksandr Volik <polskasasha@gmail.com>
2023-01-28 16:10:04 +0200

    add/create contacts' page + fix import's bugs

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import ContactForm from '../components/ContactForm/ContactForm';
+import Filter from '../components/Filter/Filter';
+import ContactList from '../components/ContactList/ContactList';
+import { fetchContacts } from '../redux/contacts/fetchApi';
+
+const Contacts = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <ContactForm />
+      <Filter />
+      <ContactList />
+    </>
+  );
+};
+
+export default Contacts;
