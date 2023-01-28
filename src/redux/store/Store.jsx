358a58ca9e4694a8de284fb013c4e1ddce122ebc
Oleksandr Volik <polskasasha@gmail.com>
2023-01-28 15:37:27 +0200

    configure work with backend + add async + redux persist

--- src/redux/store/Store.jsx
+++ src/redux/store/Store.jsx
@@ -1,12 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistedReducer } from '../contacts/contactsSlice';
 import { filterReducer } from '../contacts/FilterSlice';
+import { authReducer } from '../authentication/slice';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     contacts: persistedReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 
