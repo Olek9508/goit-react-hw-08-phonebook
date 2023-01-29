ed6c3beaf255da52add5d7229132676501aff77d
Oleksandr Volik <polskasasha@gmail.com>
2023-01-29 21:55:46 +0200

    add notify

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Formik, Field } from 'formik';
-import {
-  FormBox,
-  Label,
-  Input,
-  BtnSubmit,
-} from './ContactForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
+import { Formik, Field } from 'formik';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { addContact } from 'redux/contacts/fetchApi';
 import { selectContacts } from 'redux/contacts/Selectors';
+import { FormBox, Label, Input, BtnSubmit} from './ContactForm.styled';
+
+
 
 export const ContactForm = () => {
   const initualValues = {
     name: '',
-    phone: '',
+    number: '',
   };
 
   const contacts = useSelector(selectContacts);
@@ -24,35 +22,41 @@ export const ContactForm = () => {
     contacts.find(
       contact => values.name.toLowerCase() === contact.name.toLowerCase()
     )
-      ? alert(`${values.name} is already in contacts.`)
-      : dispatch(addContact(values));
-    resetForm();
+      ? Notify.failure(`${values.name} is already in contacts`)
+      : dispatch(addContact(values)) && resetForm();
   };
 
   return (
-    <Formik initialValues={initualValues} onSubmit={handleOnSubmit}>
-      <FormBox>
-        <Label>Name</Label>
-        <Field
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          as={Input}
-          required
-        />
-        <Label>Number</Label>
-        <Field
-          type="tel"
-          name="phone"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          as={Input}
-          required
-        />
-        <BtnSubmit type="submit">Add contact</BtnSubmit>
-      </FormBox>
-    </Formik>
+    <section>
+      <div>
+        <h2>Add contact:</h2>
+        <Formik initialValues={initualValues} onSubmit={handleOnSubmit}>
+          <FormBox>
+            <Label>Name</Label>
+            <Field
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              as={Input}
+              required
+            />
+            <Label>Number</Label>
+            <Field
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              as={Input}
+              required
+            />
+            <BtnSubmit variant="contained" type="submit">
+              Add contact
+            </BtnSubmit>
+          </FormBox>
+        </Formik>
+      </div>
+    </section>
   );
 };
 
