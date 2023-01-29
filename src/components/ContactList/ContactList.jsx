c4a4b8480702bb3b74f03877e2b91af992e2844c
Oleksandr Volik <polskasasha@gmail.com>
2023-01-29 21:49:01 +0200

    create render after add fetch function

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,32 +1,47 @@
-import {ListBox, ListItem, BtnDelete, Text } from './Contact.styled';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectContactsByName, selectLoadingStatus } from 'redux/contacts/Selectors';
-import { deleteContact } from 'redux/contacts/fetchApi';
+import { fetchContacts, deleteContact } from 'redux/contacts/fetchApi';
 import { Loader } from 'components/Loader/SpinningLoader';
+import { ListBox, ListItem, BtnDelete, Text } from './Contact.styled';
+
 
 export const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContactsByName);
   const isLoading = useSelector(selectLoadingStatus);
 
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <ListBox>
-      {isLoading && <Loader />}
-      {contacts.map(({ id, name, phone }) => (
-        <ListItem key={id}>
-          <Text>Name: {name}</Text>
-          <Text>Number: {phone}</Text>
-          <BtnDelete
-            type="button"
-            onClick={() => {
-              dispatch(deleteContact(id));
-            }}
-          >
-            Delete contact
-          </BtnDelete>
-        </ListItem>
-      ))}
-    </ListBox>
+    <section>
+      <div>
+        <h2>Contacts</h2>
+        {contacts.length === 0 ? (
+          <h2>Phonebook is empty</h2>
+        ) : (
+          <ListBox>
+            {isLoading && <Loader />}
+            {contacts.map(({ id, name, number }) => (
+              <ListItem key={id}>
+                <Text>{name}</Text>
+                <Text>{number}</Text>
+                <BtnDelete
+                  variant="contained"
+                  onClick={() => {
+                    dispatch(deleteContact(id));
+                  }}
+                >
+                  Delete contact
+                </BtnDelete>
+              </ListItem>
+            ))}
+          </ListBox>
+        )}
+      </div>
+    </section>
   );
 };
 
